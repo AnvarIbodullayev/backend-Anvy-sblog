017b1938ea28484ec2f564412d73283781695160
Anvar <61209846+AnvarIbodullayev@users.noreply.github.com>
2025-09-16 18:26:56 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.use('/uploads', express.static(join(process.cwd(), 'uploads')));
-  await app.listen(process.env.PORT ?? 3333);
+
+  // PORT env bor bo'lsa number ga o'tkaz, yo'q bo'lsa 3333
+  const port = Number(process.env.PORT) || 3333;
+
+  await app.listen(port, '0.0.0.0');
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
